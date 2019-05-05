e6bf0c4ec38609217de2d46b492a559deb52f574
Elena <helena_grin@ukr.net>
2019-05-05 23:31:10 +0300

    addd

--- src/components/TableBodyCell/TableBodyCell.jsx
+++ src/components/TableBodyCell/TableBodyCell.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import withStyles from 'react-jss';
 // import './styles.css';
+
+const styles = ({ cells }) => ({
+	cells
+});
 
 class TableBodyCell extends React.PureComponent {
 	render = () => {
-		const { children = [] } = this.props;
-		return <td className="table-body-cell__container">
+		const { children = [], classes } = this.props;
+		return <td className={classes.cells}>
 			{children}
 		</td>
 	}
 }
 
-export default TableBodyCell;
+export default withStyles(styles)(TableBodyCell);
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { BrowserRouter } from 'react-router-dom';
+//import { Provider } from 'react-redux';
+//import createStore from './store.js';
+import { BrowserRouter, Switch } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'react-jss';
 import App from './App.jsx';
+
+//const store = createStore();  //создаем store (если store один, можно это делать внизу: <Provider store={createStore()}>)
 
 const theme = {
 	palette: {
@@ -21,22 +25,48 @@ const theme = {
 		cursor: 'pointer',
 		'&:hover': {
 			color: '#fff',
-			backgroundColor: 'grey'
+			backgroundColor: 'grey',
+			outline: 'none'
 		}
 	},
 	headers: {
 		backgroundColor: 'pink',
 		textAlign: 'center',
+		fontSize: '1.5rem',
 		lineHeight: '3rem',
 		margin: 5
 	},
 	rows: {
-		margin: 10
+		lineHeight: '1.5rem'
+	},
+	cells: {
+		border: '1px solid grey',
+		"&:not(:first-child)": {
+			paddingLeft: 20
+		},
+		"&:first-child, &:nth-child(n+5)": {
+			textAlign: 'center'
+		},
+		"&:first-child input": {
+			transform: 'scale(2)',
+			verticalAlign: 'middle'
+		},
+		"&:not(:nth-child(5))": {
+			fontStyle: 'italic'
+		},
+		"&:last-child": {
+			fontWeight: 'bold'
+		}
 	}
 };
 
+//const store = createStore();
+// console.log(store);
+
 ReactDOM.render(<ThemeProvider theme={theme}>
 	<BrowserRouter>
-		<App />
+		// <Provider store={store}>
+			<App />
+		// </Provider>
 	</BrowserRouter>
 </ThemeProvider>, document.getElementById('root'));
